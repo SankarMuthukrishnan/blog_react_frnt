544af113da535b9fc6f0310621daebe5c00f2fe3
Sankar Muthukrishnan <sankar@handbuiltapps.com>
2021-08-15 20:24:47 +0530

    code is updated.

--- src/components/Pages/Blog/index.js
+++ src/components/Pages/Blog/index.js
@@ -7,7 +7,7 @@ import { required } from 'redux-form-validators';
 import { Input, Button } from '../../../components/Field';
 import { getItems, submitItem } from '../../../actions';
 import './style.css';
-
+import blog_img from '../../../assets/images/blog.jpeg';
 class Blog extends React.Component {
 
     constructor(props) {
@@ -33,11 +33,13 @@ class Blog extends React.Component {
         const { replyForm } = this.state;
         return (
             <div className={"comments-list"} key={`reply_${item._id}`} id={`comment_id_${item._id}`}>
-                <div className={"comment-content-sec"}>
-                    <span dangerouslySetInnerHTML={{ __html: item.content }}></span>
-                    {replyBtn && <button className="col" onClick={() => this.commentReply(item._id)}>reply</button>}
+                <div className={"comment-box comment-content-sec"}>
+                    <div className={"p_rel"}>
+                    <p className={'comment-content'} dangerouslySetInnerHTML={{ __html: item.content }}></p>
+                    {replyBtn && <button className="col post-btn" onClick={() => this.commentReply(item._id)}>Reply</button>}
+                    </div>
                 </div>
-                {replyForm && this.fromElement(replyForm)}
+                {replyForm == item._id && this.fromElement(replyForm)}
                 {item.child_list && item.child_list.length > 0 && item.child_list.map((chil, inDx) => (
                     this.commentsItemElement(chil, false)
                 ))}
@@ -59,17 +61,17 @@ class Blog extends React.Component {
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div className="comment-box">
-                    {parent_id &&
-                        <Field component={Input} name="parent_id" type="hidden" value={parent_id} />
-                    }
-                    <Field component={Input} name="content" validate={[required()]} label="" servererror={serverError["page-title"]} required={true} type={'editor'} />
-                    <Button name="submit" submit={true} rectBtn={``} label={"Submit"} classes={`col omment-btn`} disabled={invalid} color="#3b3f54" fetching={''} />
-                    {parent_id &&
-                        <Button name="cancel" submit={false} rectBtn={``} label={"Cancel"} classes={`col comment-btn`} onClick={() => this.setState({ replyForm: false })} />
-                    }
-
+                    <div className="p_rel">
+                        {parent_id &&
+                            <Field component={Input} name="parent_id" type="hidden" value={parent_id} />
+                        }
+                        <Field component={Input} name="content" validate={[required()]} label="" servererror={serverError["page-title"]} required={true} type={'editor'} />
+                        <Button name="submit" submit={true} rectBtn={``} label={"Submit"} classes={`col comment-btn post-btn`} disabled={invalid} color="#3b3f54" fetching={''} />
+                        {parent_id &&
+                            <Button name="cancel" submit={false} rectBtn={``} label={"Cancel"} classes={`col comment-btn post-btn cancel-btn`} onClick={() => this.setState({ replyForm: false })} />
+                        }
+                    </div>
                 </div>
-
             </form>
         )
     }
@@ -83,19 +85,21 @@ class Blog extends React.Component {
         const { replyForm } = this.state;
         return (
             <div className="middle-wrap">
-                <h3>blog title</h3>
-                <div className="image-wrap"></div>
+                <h3 className={'post-title'}>blog title</h3>
+                <img className="image-wrap" src={blog_img} />
                 <div className="comments-area">
                     <p>Comments</p>
                     <div>
                         {this.fromElement()}
                         {list && list.length > 0 && list.map((commentItem, index) => (
                             <div className={"comments-list"} key={`comment_${index}`} id={`comment_id_${commentItem._id}`}>
-                                <div className={"comment-content-sec"}>
-                                    <span dangerouslySetInnerHTML={{ __html: commentItem.content }}></span>
-                                    <button className="col" onClick={() => this.commentReply(commentItem._id)}>reply</button>
+                                <div className={"comment-box comment-content-sec"}>
+                                    <div className="p_rel">
+                                        <p className={'comment-content'} dangerouslySetInnerHTML={{ __html: commentItem.content }}></p>
+                                        <button className="col post-btn" onClick={() => this.commentReply(commentItem._id)}>Reply</button>
+                                    </div>
                                 </div>
-                                {replyForm && this.fromElement(replyForm)}
+                                {replyForm == commentItem._id && this.fromElement(replyForm)}
                                 {commentItem.child_list && commentItem.child_list.length > 0 && commentItem['child_list'].map((ch, inDx) => (
                                     this.commentsItemElement(ch)
                                 ))}
